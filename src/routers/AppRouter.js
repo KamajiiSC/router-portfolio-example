24e163402dc2440e5814b02b190b15b2f2d87765
KamajiiSC <brandonlschaen@gmail.com>
2020-05-10 00:30:30 -0400

    Set /portfolio route exact to true and made a route for portfolio/:id and set it to PortfolioItemPage

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import HomePage from '../components/HomePage';
 import NotFoundPage from '../components/NotFoundPage';
 import PortfolioPage from '../components/PortfolioPage';
+import PortfolioItemPage from '../components/PortfolioItemPage';
 import ContactPage from '../components/ContactPage';
 
 const AppRouter = () => (
@@ -12,7 +13,8 @@ const AppRouter = () => (
       <Header />
       <Switch>
         <Route path='/' component={HomePage} exact={true} />
-        <Route path='/portfolio' component={PortfolioPage}/>
+        <Route path='/portfolio' component={PortfolioPage} exact={true}/>
+        <Route path='/portfolio/:id' component={PortfolioItemPage}/>
         <Route path='/contact' component={ContactPage} />
         <Route component={NotFoundPage} />
       </Switch>
