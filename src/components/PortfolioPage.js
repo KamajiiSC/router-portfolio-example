caae525ef19b6842598b96b9e8830d5557368f7b
KamajiiSC <brandonlschaen@gmail.com>
2020-05-10 00:32:46 -0400

    Setup NavLink for ItemOne and ItemTwo and linked to :id

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -3,8 +3,8 @@ import { NavLink } from 'react-router-dom';
 
 const PortfolioPage = () => (
   <div>
-    <NavLink to="">Item One</NavLink>
-    <NavLink to="">Item Two</NavLink>
+    <NavLink to="/portfolio/1">Item One</NavLink>
+    <NavLink to="/portfolio/2">Item Two</NavLink>
   </div>
 );
 
