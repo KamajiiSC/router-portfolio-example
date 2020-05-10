d709ea7e6cd26a3456a477c2c5d08be0baf1f7ae
KamajiiSC <brandonlschaen@gmail.com>
2020-05-09 23:59:49 -0400

    Inserted component ContactPage in AppRouter and fixed Route paths

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import React from 'react';
-import Header from '../components/Header.js';
+import Header from '../components/Header';
 import HomePage from '../components/HomePage';
 import NotFoundPage from '../components/NotFoundPage';
+import ContactPage from '../components/ContactPage';
 
 const AppRouter = () => (
     <BrowserRouter>
@@ -10,8 +11,8 @@ const AppRouter = () => (
       <Header />
       <Switch>
         <Route path='/' component={HomePage} exact={true} />
-        <Route path='portfolio' />
-        <Route path='contact' />
+        <Route path='/portfolio' />
+        <Route path='/contact' component={ContactPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
